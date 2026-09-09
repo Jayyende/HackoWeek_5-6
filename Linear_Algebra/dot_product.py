8ebaeef742a3544f8bd6cc6eb125f839e952ac8b
Jayyende <jayyende7875@gmail.com>
2026-09-09 10:46:28 +0530

    Add dot product calculation in dot_product.py

---
+++ Linear_Algebra/dot_product.py
@@ -0,0 +1,21 @@
+# dot_product.py
+
+import numpy as np
+
+print("=== DOT PRODUCT ===\n")
+
+A = np.array([1, 2, 3])
+B = np.array([4, 5, 6])
+
+print("Vector A:", A)
+print("Vector B:", B)
+
+dot = np.dot(A, B)
+
+print("\nDot Product:")
+print(dot)
+
+print("\nInterpretation:")
+print("A higher value generally indicates greater similarity.")
+
+print("\n=== PROGRAM COMPLETED ===")
