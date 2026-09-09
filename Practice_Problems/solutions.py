176c79ecd79a31c792bd1291206e3637d5deadf4
Jayyende <jayyende7875@gmail.com>
2026-09-09 11:28:12 +0530

    Add solutions summary for various math operations

---
+++ Practice_Problems/solutions.py
@@ -0,0 +1,30 @@
+# solutions.py
+
+print("=== SOLUTIONS SUMMARY ===\n")
+
+print("Vector Magnitude [3,4] = 5")
+
+print("\nVector Addition")
+print("[2,3] + [4,5] = [6,8]")
+
+print("\nDot Product")
+print("[1,2] . [3,4] = 11")
+
+print("\nMatrix Multiplication")
+print("""
+|1 2|   |5 6|
+|3 4| x |7 8|
+
+=
+|19 22|
+|43 50|
+""")
+
+print("Derivative of x² = 2x")
+
+print("Gradient of x²+y² = [2x,2y]")
+
+print("Chain Rule")
+print("d/dx[(x²+1)³] = 6x(x²+1)²")
+
+print("\n=== END ===")
